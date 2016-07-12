a5296d6ff284fb1610fec145a7a173d76d042847
Fritz <fre.healy@gmail.com>
2016-07-12 15:13:21 -0400

    rearranged custom post types

--- inc/post-types.php
+++ inc/post-types.php
@@ -8,44 +8,13 @@ function js_custom_init()
 	// Register the Homepage Slides
   
      $labels = array(
-	'name' => _x('Projects', 'post type general name'),
-    'singular_name' => _x('Project', 'post type singular name'),
-    'add_new' => _x('Add New', 'Project'),
-    'add_new_item' => __('Add New Project'),
-    'edit_item' => __('Edit Projects'),
-    'new_item' => __('New Project'),
-    'view_item' => __('View Projects'),
-    'search_items' => __('Search Projects'),
-    'not_found' =>  __('No Projects found'),
-    'not_found_in_trash' => __('No Projects found in Trash'), 
-    'parent_item_colon' => '',
-    'menu_name' => 'Projects'
-  );
-  $args = array(
-	'labels' => $labels,
-    'public' => true,
-    'publicly_queryable' => true,
-    'show_ui' => true, 
-    'show_in_menu' => true, 
-    'query_var' => true,
-    'rewrite' => true,
-    'capability_type' => 'post',
-    'has_archive' => false, 
-    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
-    'menu_position' => 20,
-    'supports' => array('title','editor','thumbnail'),
-	
-  ); 
-  register_post_type('project',$args); // name used in query
-  
-  $labels = array(
 	'name' => _x('Services', 'post type general name'),
     'singular_name' => _x('Service', 'post type singular name'),
     'add_new' => _x('Add New', 'Service'),
     'add_new_item' => __('Add New Service'),
-    'edit_item' => __('Edit Services'),
+    'edit_item' => __('Edit Service'),
     'new_item' => __('New Service'),
-    'view_item' => __('View Services'),
+    'view_item' => __('View Service'),
     'search_items' => __('Search Services'),
     'not_found' =>  __('No Services found'),
     'not_found_in_trash' => __('No Services found in Trash'), 
@@ -73,25 +42,3 @@ function js_custom_init()
 	// and here
 
 } // close custom post type
-	
-	/*
-   ##############################################
-	Custom Taxonomies
-   */
-   add_action( 'init', 'build_taxonomies', 0 );
-    
-   function build_taxonomies() {
-   // cusotm tax
-	  register_taxonomy( 'project_type', 'project',
-	 array( 
-	'hierarchical' => true, // true = acts like categories false = acts like tags
-	'label' => 'Project Type', 
-	'query_var' => true, 
-	'rewrite' => true ,
-	'show_admin_column' => true,
-	'public' => true,
-	'rewrite' => array( 'slug' => 'project-type' ),
-	'_builtin' => true
-	) );
-	
-   } // End build taxonomies
--- inc/theme.php
+++ inc/theme.php
@@ -103,10 +103,10 @@ add_action( 'init', 'my_theme_add_editor_styles' );
 function change_post_menu_label() {
     global $menu;
     global $submenu;
-    $menu[5][0] = 'News';
-    $submenu['edit.php'][5][0] = 'News';
-    $submenu['edit.php'][10][0] = 'Add News Item';
-    //$submenu['edit.php'][15][0] = 'Status'; // Change name for categories
+    $menu[5][0] = 'Projects';
+    $submenu['edit.php'][5][0] = 'Projects';
+    $submenu['edit.php'][10][0] = 'Add Project';
+    $submenu['edit.php'][15][0] = 'Project Type'; // Change name for categories
     //$submenu['edit.php'][16][0] = 'Labels'; // Change name for tags
     echo '';
 }
@@ -114,17 +114,20 @@ function change_post_menu_label() {
 function change_post_object_label() {
         global $wp_post_types;
         $labels = &$wp_post_types['post']->labels;
-        $labels->name = 'News';
-        $labels->singular_name = 'News Item';
-        $labels->add_new = 'Add News Item';
-        $labels->add_new_item = 'Add News Item';
-        $labels->edit_item = 'Edit News Item';
-        $labels->new_item = 'News Item';
-        $labels->view_item = 'View News Item';
-        $labels->search_items = 'Search News';
-        $labels->not_found = 'No News found';
-        $labels->not_found_in_trash = 'No News found in Trash';
-    }
+        $labels->name = 'Projects';
+        $labels->singular_name = 'Project';
+        $labels->add_new = 'Add Project';
+        $labels->add_new_item = 'Add Project';
+        $labels->edit_item = 'Edit Project';
+        $labels->new_item = 'Project';
+        $labels->view_item = 'View Project';
+        $labels->search_items = 'Search Projects';
+        $labels->not_found = 'No Projects found';
+        $labels->not_found_in_trash = 'No Projects found in Trash';
+    
+				//remove post_tags for all post types
+				register_taxonomy('post_tag','');
+		}
 add_action( 'init', 'change_post_object_label' );
 add_action( 'admin_menu', 'change_post_menu_label' );
 
