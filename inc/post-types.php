5efb7b547005ca710d0cf42d27cbae3724918465
Fritz <fre.healy@gmail.com>
2016-07-12 14:12:55 -0400

    setting up custom post types

--- inc/post-types.php
+++ inc/post-types.php
@@ -8,18 +8,18 @@ function js_custom_init()
 	// Register the Homepage Slides
   
      $labels = array(
-	'name' => _x('Slides', 'post type general name'),
-    'singular_name' => _x('Slide', 'post type singular name'),
-    'add_new' => _x('Add New', 'Slide'),
-    'add_new_item' => __('Add New Slide'),
-    'edit_item' => __('Edit Slides'),
-    'new_item' => __('New Slide'),
-    'view_item' => __('View Slides'),
-    'search_items' => __('Search Slides'),
-    'not_found' =>  __('No Slides found'),
-    'not_found_in_trash' => __('No Slides found in Trash'), 
+	'name' => _x('Projects', 'post type general name'),
+    'singular_name' => _x('Project', 'post type singular name'),
+    'add_new' => _x('Add New', 'Project'),
+    'add_new_item' => __('Add New Project'),
+    'edit_item' => __('Edit Projects'),
+    'new_item' => __('New Project'),
+    'view_item' => __('View Projects'),
+    'search_items' => __('Search Projects'),
+    'not_found' =>  __('No Projects found'),
+    'not_found_in_trash' => __('No Projects found in Trash'), 
     'parent_item_colon' => '',
-    'menu_name' => 'Slides'
+    'menu_name' => 'Projects'
   );
   $args = array(
 	'labels' => $labels,
@@ -33,13 +33,65 @@ function js_custom_init()
     'has_archive' => false, 
     'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
     'menu_position' => 20,
-    'supports' => array('title','editor','custom-fields','thumbnail'),
+    'supports' => array('title','editor','thumbnail'),
 	
   ); 
-  register_post_type('slides',$args); // name used in query
+  register_post_type('project',$args); // name used in query
   
-  // Add more between here
+  $labels = array(
+	'name' => _x('Services', 'post type general name'),
+    'singular_name' => _x('Service', 'post type singular name'),
+    'add_new' => _x('Add New', 'Service'),
+    'add_new_item' => __('Add New Service'),
+    'edit_item' => __('Edit Services'),
+    'new_item' => __('New Service'),
+    'view_item' => __('View Services'),
+    'search_items' => __('Search Services'),
+    'not_found' =>  __('No Services found'),
+    'not_found_in_trash' => __('No Services found in Trash'), 
+    'parent_item_colon' => '',
+    'menu_name' => 'Services'
+  );
+  $args = array(
+	'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
+    'menu_position' => 20,
+    'supports' => array('title','editor','thumbnail'),
+	
+  ); 
+  register_post_type('service',$args); // name used in query
   
-  // and here
-  
-  } // close custom post type
+	// add more between here
+	// and here
+
+} // close custom post type
+	
+	/*
+   ##############################################
+	Custom Taxonomies
+   */
+   add_action( 'init', 'build_taxonomies', 0 );
+    
+   function build_taxonomies() {
+   // cusotm tax
+	  register_taxonomy( 'project_type', 'project',
+	 array( 
+	'hierarchical' => true, // true = acts like categories false = acts like tags
+	'label' => 'Project Type', 
+	'query_var' => true, 
+	'rewrite' => true ,
+	'show_admin_column' => true,
+	'public' => true,
+	'rewrite' => array( 'slug' => 'project-type' ),
+	'_builtin' => true
+	) );
+	
+   } // End build taxonomies
