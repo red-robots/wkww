eba12d484b0fdea2be577590f00dee578e10af3c
Fritz <fre.healy@gmail.com>
2016-07-28 16:43:01 -0400

    styling and tempalating the 404 page

--- 404.php
+++ 404.php
@@ -11,44 +11,17 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
+			<div class="error-404 not-found">
 				<header>
 					<h1 class="title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'acstarter' ); ?></h1>
 				</header>
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'acstarter' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( acstarter_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'acstarter' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php endif; ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+				<section class="copy copy-404">
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below?', 'acstarter' ); ?></p>
+					<nav class="sitemap">
+						<?php wp_nav_menu( array( 'theme_location' => 'sitemap' ) ); ?>
+					</nav>
+				</section><!-- .page-content -->
+			</div><!-- .error-404 -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
