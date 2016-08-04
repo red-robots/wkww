0e0d465196c485740b84cae9c45ba02e6de5457e
Fritz <fre.healy@gmail.com>
2016-08-04 15:39:41 -0400

    ditto

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -109,7 +109,6 @@ jQuery(document).ready(function ($) {
 	$(window).on('ready resize',function(){
 		$('.size-4-2-5').each(function(){	
 			var $this = $(this);
-			console.log($this);
 			$this.css({
 				"height": Number($this.width())*2.5/4+"px"
 			});
